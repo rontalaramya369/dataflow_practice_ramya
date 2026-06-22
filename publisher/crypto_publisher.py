2f77500fb2974dbd6692424e386b4bc8b12076b2
rontalaramya369 <rontalaramya369@gmail.com>
2026-06-22 13:16:24 +0530

    publisher changed

--- publisher/crypto_publisher.py
+++ publisher/crypto_publisher.py
@@ -1,49 +1,29 @@
 import requests
-import json
 import time
 from datetime import datetime
 
-from google.cloud import pubsub_v1
-
-
-PROJECT_ID = "project-8a611ce5-dc75-4904-b12"
-
-TOPIC_ID = "crypto-topic"
-
-
-publisher = pubsub_v1.PublisherClient()
-
-topic_path = publisher.topic_path(PROJECT_ID, TOPIC_ID)
-
-
 while True:
 
-    url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
-
-    response = requests.get(url)
+    response = requests.get(
+        "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+    )
 
     data = response.json()
 
-    message = {
+    print("API Response:", data)
 
-        "coin":"bitcoin",
+    if "bitcoin" in data:
 
-        "price_usd":data["bitcoin"]["usd"],
+        record = {
+            "coin": "bitcoin",
+            "price_usd": data["bitcoin"]["usd"],
+            "last_updated": datetime.utcnow().isoformat()
+        }
 
-        "last_updated":datetime.utcnow().isoformat()
+        print(record)
 
-    }
+    else:
 
-
-    publisher.publish(
-
-        topic_path,
-
-        json.dumps(message).encode("utf-8")
-
-    )
-
-
-    print(message)
+        print("Bitcoin data not found")
 
     time.sleep(5)
